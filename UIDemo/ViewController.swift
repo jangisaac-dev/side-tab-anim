653c34fe2522810855affd0b572487b142627916
isaacjang-m2 <dltkr717@gmail.com>
2023-09-02 16:57:56 +0900

    Initial Commit

---
+++ UIDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIDemo
+//
+//  Created by Isaac Jang on 2023/09/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
